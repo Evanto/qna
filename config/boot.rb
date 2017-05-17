6d1409003099c087f396f4efb6a52d9b74993c64
Eva Medvedeva <ohmywob@gmail.com>
2017-05-17 17:38:57 +0300

    init configured repo

---
+++ config/boot.rb
@@ -0,0 +1,13 @@
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+
+require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+require 'rails/commands/server'
+
+module Rails
+  class Server
+    def default_options
+      super.merge({Port: 3000, Host: '0.0.0.0'})
+    end
+  end
+end
