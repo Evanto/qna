52627dad741ba0a8b5d472cd902ec98272d44971
Eva Medvedeva <ohmywob@gmail.com>
2017-05-18 17:51:10 +0300

    add foreign key, tests pass

--- db/migrate/20170517182538_create_answers.rb
+++ db/migrate/20170517182538_create_answers.rb
@@ -1,7 +1,6 @@
 class CreateAnswers < ActiveRecord::Migration[5.1]
   def change
     create_table :answers do |t|
-      t.references :question, foreign_key: true
       t.text :body
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170517182538) do
+ActiveRecord::Schema.define(version: 20170518144749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20170517182538) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "question_id"
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -28,4 +30,5 @@ ActiveRecord::Schema.define(version: 20170517182538) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "answers", "questions"
 end
