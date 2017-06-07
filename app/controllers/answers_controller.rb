ec86f28b1f8aa6a1b7901342ce125411133cbc22
Eva Medvedeva <ohmywob@gmail.com>
2017-06-07 12:46:26 +0300

    add answer and delete answer work fine

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,10 @@
 class AnswersController < ApplicationController
-  before_action :authenticate_user!
-  before_action :load_question
+  before_action :authenticate_user!, except: [:show]
+  before_action :load_question, only: [:create, :new]
+  before_action :load_answer, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
 
   def new
     @answer = @question.answers.new
@@ -8,13 +12,10 @@ class AnswersController < ApplicationController
 
   def create
     @answer = @question.answers.new(answer_params)
-    @question = current_user.questions.new(question_params)
-    @answer.user_id = current_user.id
-    #answer = @question.answers.new(answer_params.merge(user_id: current_user.id))
-    #@answer = current_user.answers.new(answer_params)
+    @answer.user = current_user # было @answer.user_id = current_user.id
     if @answer.save
       flash[:notice] = 'Your answer was successfully created.'
-      redirect_to question_path(@question, @answer)
+      redirect_to @answer.question
     else
       flash[:alert] = 'Something went wrong, please try again.'
       render :new
@@ -22,11 +23,13 @@ class AnswersController < ApplicationController
   end
 
   def destroy
-    @answer = @questions.answers.find(params[:id])
-    if current_user.author_of?(@answer)
+    @question = @answer.question
+    if current_user.id == @answer.user_id
       @answer.destroy
+      redirect_to @question, notice: 'Answer successfully deleted'
+    else
       flash[:notice] = 'Your answer was successfully deleted.'
-      redirect_to question_path
+      redirect_to @question
     end
   end
 
@@ -36,7 +39,12 @@ class AnswersController < ApplicationController
     @question = Question.find(params[:question_id])
   end
 
+  def load_answer # добавлено
+    @answer = Answer.find(params[:id])
+  end
+
   def answer_params
     params.require(:answer).permit(:body)
   end
+
 end
