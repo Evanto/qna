b6e6e88cb580a3d24e0a375490a0559bcf373649
Eva Medvedeva <ohmywob@gmail.com>
2017-08-18 14:14:02 +0300

    removed attachable form 2 tests of attachments_controller_spec.rb

--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -2,21 +2,21 @@ require 'rails_helper'
 
 RSpec.describe AttachmentsController, type: :controller do
   let(:question)  { create(:question) }
-  let(:answer)    { create(:answer, question: question, user: @user) }
   let(:answer2)   { create(:answer, question: question) }
 
   describe 'DELETE #destroy' do
     sign_in_user
+    let(:answer)    { create(:answer, question: question, user: @user) }
     let!(:attachment) { create(:attachment, attachable: answer) }
 
     context 'answer of author' do
 
       it 'deletes attach of author' do
-        expect { delete :destroy, params: { attachable: answer, id: attachment.id, format: :js }  }.to change(Attachment, :count).by(-1)
+        expect { delete :destroy, params: { id: attachment.id, format: :js }  }.to change(Attachment, :count).by(-1)
       end
 
       it 'renders destroy template' do
-        delete :destroy, params: { attachable: answer, id: attachment.id, format: :js }
+        delete :destroy, params: { id: attachment.id, format: :js }
         expect(response).to render_template :destroy
       end
     end
